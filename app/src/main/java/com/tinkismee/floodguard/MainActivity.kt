6710f73727ab68af8bde1359ed93fdfeb9a4c8cb
tinkismeeee <tinhisme1@gmail.com>
2026-01-10 14:09:49 +0700

    Add bottom nav bar

--- app/src/main/java/com/tinkismee/floodguard/MainActivity.kt
+++ app/src/main/java/com/tinkismee/floodguard/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavBar : BottomNavigationView
+    private lateinit var fragmentContainer : FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initVars()
+    }
+
+
+    private fun initVars() {
+        bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        fragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
+    }
+
+    private fun changeFragment() {
+
     }
 }
